30e93f7c4ec6dda56465fa48c278f95d11fe584b
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2021-01-21 21:20:52 +0100

    nsigma in convolution

--- src/convolution.jl
+++ src/convolution.jl
@@ -1,6 +1,6 @@
 
-conv_with_gauss(e,f,σ) = quadgk(y->f(y)*standardgauss(e-y,σ), e-3σ, e+3σ)[1]
-conv_with_gauss(d::pdf,σ) = pdf((e;p)->conv_with_gauss.(e,x->func(d,x; p=p),σ); p=pars(d), lims=lims(d))
+conv_with_gauss(e,f,σ; nσ=3) = quadgk(y->f(y)*standardgauss(e-y,σ), e-nσ*σ, e+nσ*σ)[1]
+conv_with_gauss(d::pdf,σ; nσ=3) = pdf((e;p)->conv_with_gauss.(e,x->func(d,x; p=p),σ; nσ=nσ); p=pars(d), lims=lims(d))
 
 function conv_with_gauss_sampling(e,f,σ; Ns=10)
     points = range(-3σ, +3σ, length=Ns)
