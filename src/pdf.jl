5db1bbe37d7d3630fa91a560d6c7fa7adb059367
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2021-04-20 10:30:02 +0200

    cleaned types

--- src/pdf.jl
+++ src/pdf.jl
@@ -4,19 +4,19 @@ const uTAS = Union{Tuple,Array{Symbol}}
 ∅ = NamedTuple()
 freepars(f::Function) = ∅
 #
-freepars(d::T where T<:FunctionWithParameters) = freepars(pars(d))
-fixedpars(d::T where T<:FunctionWithParameters) = fixedpars(pars(d))
+freepars(d::FunctionWithParameters) = freepars(pars(d))
+fixedpars(d::FunctionWithParameters) = fixedpars(pars(d))
 # 
 # properties
-npars(d::T where T<:FunctionWithParameters) = length(freepars(d))
-v2p(v,d::T where T<:FunctionWithParameters) = NamedTuple{keys(freepars(d))}(v)
-p2v(p,d::T where T<:FunctionWithParameters) = [getproperty(p, k) for k in keys(freepars(d))]
-p2v(  d::T where T<:FunctionWithParameters) = p2v(freepars(d), d)
+npars(d::FunctionWithParameters) = length(freepars(d))
+v2p(v,d::FunctionWithParameters) = NamedTuple{keys(freepars(d))}(v)
+p2v(p,d::FunctionWithParameters) = [getproperty(p, k) for k in keys(freepars(d))]
+p2v(  d::FunctionWithParameters) = p2v(freepars(d), d)
 
-fixpars(d::T where T<:FunctionWithParameters, s::T where T<:uTAS) =
+fixpars(d::FunctionWithParameters, s::uTAS) =
         fixpars(d, selectpars(freepars(d), s))
-fixpar(d::T where T<:FunctionWithParameters, s::Symbol) =  fixpars(d, (s,))
-fixpar(d::T where T<:FunctionWithParameters, s::Symbol, v::T where T<:Real) =  fixpars(d, nt(s,v))
+fixpar(d::FunctionWithParameters, s::Symbol) =  fixpars(d, (s,))
+fixpar(d::FunctionWithParameters, s::Symbol, v::Real) =  fixpars(d, nt(s,v))
 
 #                  _|      _|_|  
 #  _|_|_|      _|_|_|    _|      
@@ -45,7 +45,7 @@ fixedshapepdf(f, lims) = pdf((x;p)->f(x); lims=lims, p=∅)
 lims(d::pdf) = d.lims
 pars(d::pdf) = d.p
 func(d) = d.f
-func(d,x; p) = d.f(x;p=p)
+func(d,x; p=pars(d)) = d.f(x;p=p)
 #
 # 
 normalizationintegral(d::pdf; p=freepars(d)) = quadgk(x->func(d,x; p=p), lims(d)...)[1]
@@ -66,8 +66,8 @@ function (d::pdf)(x; p=freepars(d), norm_according_to=d)
     return func(d,x; p=allp) / normalization
 end
 (d::pdf)(x, v) = d(x; p=v2p(v,d))
-func(d,x::Array{T,N} where {T,N}; p) = func.(Ref(d), x; p=p)
-func(d,x::AbstractRange{T} where T; p) = func.(Ref(d), x; p=p)
+func(d,x::AbstractArray{T,N} where {T,N}; p=pars(d)) = func.(Ref(d), x; p=p)
+func(d,x::AbstractRange{T} where T; p=pars(d)) = func.(Ref(d), x; p=p)
 
 # fix parameters
 fixpars(d::pdf, args::NamedTuple) = copy(d, fixpars(pars(d), args))
