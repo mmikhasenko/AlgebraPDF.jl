bfb89d3976f9135106d0e84f108d9058a07bb291
Misha Mikhasenko <mikhail.mikhasenko@gmail.com>
2021-07-22 22:53:23 +0200

    add fixed seed to generation

--- test/testfit.jl
+++ test/testfit.jl
@@ -1,8 +1,11 @@
 using AlgebraPDF
 using Test
+using Random
+using Measurements
 
 @testset "Fit gauss with Minuit" begin 
     d0 = Normalized(FGauss((μ=1.1, σ=0.3)), (-3, 5))
+    Random.seed!(1234)
     data = randn(1000)
     fs0 = fit(d0, data, MigradAndHesse())
 
@@ -19,6 +22,7 @@ end
 @testset "Extended Likelihood fit" begin 
 
     d = Normalized(FGauss((μ=1.1,σ=0.1)), (-2, 3))
+    Random.seed!(4321)
     data = filter(x->inrange(x,lims(d)), randn(1000) .+ 0.5)
 
     fr = fit(d, data)
@@ -41,17 +45,18 @@ end
 # Optim often fails with `isfinite(phi_c) && isfinite(dphi_c)` 
 # removed from the tests
 # 
-# @testset "Fit gauss with Optim" begin 
-    # d0 = Normalized(FGauss((μ=0.01, σ=0.9)), (-3, 5))
-    # data = randn(1000)
-    # fs0 = fit(d0, data, BFGSApproxHesse())
+@testset "Fit gauss with Optim" begin 
+    d0 = Normalized(FGauss((μ=0.01, σ=0.9)), (-3, 5))
+    Random.seed!(1234)
+    data = randn(1000)
+    fs0 = fit(d0, data, BFGSApproxHesse())
 
-    # @test keys(fs0.parameters) == (:μ,:σ)
-    # @test typeof(Tuple(fs0.measurements)) <: Tuple{Vararg{Measurement}}
+    @test keys(fs0.parameters) == (:μ,:σ)
+    @test typeof(Tuple(fs0.measurements)) <: Tuple{Vararg{Measurement}}
 
-    # d1 = fixpar(Normalized(FGauss(Ext(μ=0.01, σ=0.9)), (-3, 5)), :σ)
-    # fs1 = fit(d1, data, BFGSApproxHesse())
+    d1 = fixpar(Normalized(FGauss(Ext(μ=0.01, σ=0.9)), (-3, 5)), :σ)
+    fs1 = fit(d1, data, BFGSApproxHesse())
 
-    # @test keys(fs1.parameters) == (:μ,)
-    # @test pars(fs1.best_model).σ == 0.9
-# end
+    @test keys(fs1.parameters) == (:μ,)
+    @test pars(fs1.best_model).σ == 0.9
+end
